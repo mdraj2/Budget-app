7ed76866c8abfa617fef0683c9185f6c53802866
Malith <malith.rajapakse@gmail.com>
2019-07-06 23:56:14 +1000

    The event handling events fuction has been appended to the controller IIFE and a button has been added to the HTML DOM to add entries. Two event handlers were added. One will take into account this button being clicked and the other will do the same but listen to the enter keystroke

--- script.js
+++ script.js
@@ -18,18 +18,7 @@ should be interested in doing one thing only. They dont even know the other one 
 
 // this will handle the data 
 var budgetController = (function(){
-	var x = 23;
-	/*this is a private add function. This means that we cannot access this from
-	the outside*/
-	var add = function(a){
-		return x + a;
-	}
-
-	return {
-		publicTest: function(b) {
-			return add(b);
-		}
-	}
+	//some code later
 })();
 
 // this will handle the view
@@ -37,16 +26,29 @@ var UIController = (function(){
 	// some code later
 })();
 
-/*we can pass arguements here. So we tell the main controller about the budget and UI controllers
-the reason we do this is because if we change the name of one of the controller then
-we only need to change once over here*/
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-	var z = budgetCtrl.publicTest(5)
-	// get into the habit of returning objects
-	return {
-		anotherPublic: function () {
-			console.log(z);
-		}
+
+	var ctrlAddItem = function() {
+		console.log('passed');
+		// this will be shared by both events
+		// Get the field input data
+		// Add the item to the budget controller
+		// Add the item to the user interace 
+		// calculate the budget
+		// Display the budget on UI
+
 	}
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+	// we also will handle the keypress as well. This will happen on the global event space
+	// e is the event which will be automatically sent by the browser
+	// keycode 13 is the return keyword
+	document.addEventListener('keypress',function(e){
+		if(e.keyCode == 13 || e.which === 13){
+			ctrlAddItem();
+		}
+	})
 
 })(budgetController,UIController);
